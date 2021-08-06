12b4e748afcd52fd6b179af4c7b3aaed7d089252
EngKwamePhilip <kwaoizybusiness@gmail.com>
2021-08-06 17:01:20 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const API_KEY = "adc8e3ff6947476e9b7bcfcc20a5e01b"
+  const [search, setSearch] = useState("https://www.brescadc.com/");
+  const [img, setImg] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const URL = `https://api.apiflash.com/v1/urltoimage?access_key=${API_KEY}&url=${search}&full_page="true"&fresh="true"`;
+
+  const getSreenshot = async() => {
+    setSearch("");
+    setError(false);
+    setLoading(true);
+    const response = await fetch(URL);
+    if (response.ok){
+      setImg(response);
+      setLoading(false);
+    }
+    else{
+      setError(true);
+    }
+  }
+
+  const searchScreenShots = (e) => {
+    e.preventDefault();
+    getSreenshot();
+  };
+
+  useEffect(()=>{
+    setSearch("");
+    getSreenshot();
+  },[])
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="container">
+          <form onSubmit={searchScreenShots}>
+            <input type="text"
+            autoFocus
+              value={search}
+              onChange={(e)=>setSearch(e.target.value)}
+            />
+            <button type="submit">Take Screenshot</button>
+          </form>
+        </div>
+      </nav>
+
+      <div className="hero">
+        {!loading && !error ? (
+          <div className="container">
+            {img && (
+            <a href={img.url} target="_blank">
+              <img src={img.url} alt="background" />   
+            </a>
+          )} 
+          </div>
+          ) : !error && loading ? (
+            <div className="loading"></div> 
+          ): error ? (
+            <div className="container">
+              <h2>Please Enter a Valid URL</h2>
+            </div>
+          ) : (
+            ""
+          )}
+      </div>
     </div>
   );
 }
